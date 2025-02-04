e7c16fb2921f27b104f5a78f36c90e478039ea00
Tacako <iberarmanosg@gmail.com>
2025-02-03 19:57:26 -0600

    Update - Más eficiente y ahora con envio de la captura de pantalla

--- ServiceCheckRelase.py
+++ ServiceCheckRelase.py
@@ -11,34 +11,43 @@ def find_and_click(image_path, confidence=0.7):
     return False
 
 
-def send_whatsapp_message(counter):
+def send_whatsapp_message():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    message = f"*SERVICO EN PLATAFORMA*"
+    message = f"*SERVICIO EN PLATAFORMA*"
     pyautogui.typewrite(message)
     pyautogui.hotkey('alt', 'enter')
     message = f"Fecha y hora: {current_time}"
     pyautogui.typewrite(message)
-    pyautogui.hotkey('alt', 'enter')
-    time.sleep(0.5)
-    message = f"Numero de Notificaciones: {counter}"
-    pyautogui.typewrite(message)
-    pyautogui.press('enter')
-
-
-
-counter = 0
-while True:
+    pyautogui.hotkey('ctrl', 'v')
     time.sleep(1)
+    pyautogui.press('enter')
+    time.sleep(2)
+
+def take_screen_shoot():
+    pyautogui.hotkey('win', 'shift', 's')
+    # Coordenadas iniciales
+    start_x, start_y = 792, 253
+    # Coordenadas finales
+    end_x, end_y = 1569, 815
+    pyautogui.moveTo(start_x, start_y)
+    time.sleep(1)
+    pyautogui.mouseDown()
+    pyautogui.moveTo(end_x, end_y)
+    pyautogui.mouseUp()
+    time.sleep(1)
+
+while True:
     try:
-        no_ace_image = pyautogui.locateOnScreen('Resources/noace.png')
+        no_ace_image = pyautogui.locateOnScreen('Resources/noace.png', confidence= 0.7)
         if no_ace_image:
-            print(pyautogui.center(no_ace_image))
-
+            find_and_click('Resources/noace.png')
+            time.sleep(4)
+            pyautogui.scroll(10)
+            take_screen_shoot()
             if find_and_click('Resources/WhatsApp.png'):
-                counter += 1
-                send_whatsapp_message(counter)
+                send_whatsapp_message()
                 find_and_click('Resources/ListaDeServicios.png')
-                time.sleep(60)
-
+                pyautogui.hotkey('alt', 'left')
+                time.sleep(50.4)
     except pyautogui.ImageNotFoundException:
-        time.sleep(0.1)
+        pass
